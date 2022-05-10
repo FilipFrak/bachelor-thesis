e73ca3a8f381aa26f64d761b8618aea7c31133c6
FilipFrak <49619142+FilipFrak@users.noreply.github.com>
2022-05-10 22:06:40 +0200

    Hot fix.

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -2,7 +2,7 @@ import gdown
 from decouple import config
 
 URL1 = config('URL1')
-URL2 = config('URL1')
+URL2 = config('URL2')
 dest = r'../../data/raw' 
 
 gdown.download_folder(URL1, output=dest, quiet=True, use_cookies=True)
